ee8455aab20c052b74bdf8e63757d05aad644a05
Ukeme Bassey <basseyukemme@gmail.com>
2024-05-10 15:47:51 +0100

    worked on search header nav

---
+++ src/components/search/search-collection/search-collection.component.jsx
@@ -0,0 +1,13 @@
+import './search-collection.styles.css';
+
+
+
+const SearchCollection = () => {
+	return (
+		<div className='search-collection'>
+
+		</div>
+	);
+}
+
+export default SearchCollection;
--- src/components/search/search-header/search-header.component.jsx
+++ src/components/search/search-header/search-header.component.jsx
@@ -6,6 +6,17 @@ const SearchHeader = () => {
 	return (
 		<div className='search-header'>
 			<h1 className='search-header__title'><span>Results for</span> django middleware</h1>
+			<div className='search-header__nav'>
+				<div className='search-header__nav--item active'>
+					<span>Stories</span>
+				</div>
+				<div className='search-header__nav--item'>
+					<span>People</span>
+				</div>
+				<div className='search-header__nav--item'>
+					<span>Topics</span>
+				</div>
+			</div>
 		</div>
 	);
 }
